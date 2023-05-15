e19d554daa64c4fc797e455e8d5f54e056c4862e
deimerhdz <deimerhdz21@gmail.com>
2023-05-15 09:46:23 -0500

    feat(backend) add base project

---
+++ src/test/java/com/dhernandez/fastfood/FastFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhernandez.fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
