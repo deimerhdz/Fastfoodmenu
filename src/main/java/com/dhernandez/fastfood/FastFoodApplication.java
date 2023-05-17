593cc6761ff981b6a86c3693eb2bc53b7f823eb6
deimerhdz <deimerhdz21@gmail.com>
2023-05-17 15:34:34 -0500

    Add swagger dependecy

--- src/main/java/com/dhernandez/fastfood/FastFoodApplication.java
+++ src/main/java/com/dhernandez/fastfood/FastFoodApplication.java
@@ -2,6 +2,11 @@ package com.dhernandez.fastfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class FastFoodApplication {
@@ -9,5 +14,16 @@ public class FastFoodApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FastFoodApplication.class, args);
 	}
+
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fast-foop Spring Boot 3 API -------")
+                        .version("0.11")
+                        .description("api documentation 3 with Swagger")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 
 }
